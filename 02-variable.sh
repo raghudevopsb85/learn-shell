6d0c05a2a84f4d06b8b8f022058ea7b43a66df3d
Raghu K <raghuk.vit@gmail.com>
2025-06-24 08:31:51 +0530

    RoboShop Code

--- 02-variable.sh
+++ 02-variable.sh
@@ -14,9 +14,18 @@ echo Value of x - ${x}
 #   LHS    script    RHS
 
 # BEFORE SCRIPT - Environment Variables
-# LHS
-# RHS           - Special Variables
 
 
+
+# On cli , I send the variable using the following command.
+# export a_env=10000
 echo Env Variable - a_env - ${a_env}
+
+
+# LHS
+# On cl
+echo LHS variable a_lhs - ${a_lhs}
+
+
+# RHS           - Special Variables
 
