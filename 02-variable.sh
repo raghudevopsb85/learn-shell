70e1e78705e615865d7817eb3ae6fc965b5c58e9
Raghu K <raghuk.vit@gmail.com>
2025-06-24 08:03:47 +0530

    RoboShop Code

---
+++ 02-variable.sh
@@ -0,0 +1,13 @@
+# If we assign a name to a set of data then it is called as variable
+
+# Syntax: var=data
+
+x=100
+# Access the variable - $var or ${var}
+
+echo Value of x - $x
+echo Value of x - ${x}
+
+
+
+
