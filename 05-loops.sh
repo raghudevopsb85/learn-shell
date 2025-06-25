cebb2ff32ebe777cca232aa60b0b4d1b242b391a
Raghu K <raghuk.vit@gmail.com>
2025-06-25 08:07:52 +0530

    RoboShop Code

---
+++ 05-loops.sh
@@ -0,0 +1,14 @@
+# if we need to iterate same commands again and again we use loops.
+# while loop  -> This works on expressions we discussed in conditions
+# for  loop -> This works on inputs
+
+for fruit in apple banana orange; do
+  echo Fruit Name - $fruit
+done
+
+x=10
+while [ $x -gt 0 ]; do
+  echo Something
+  x=$(($x-1))
+done
+
